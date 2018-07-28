646a47563a7191b2a2fa30fe7c091cf8185f0ea1
djc74 <davidcooper974@gmail.com>
2018-07-28 22:44:57 +0100

    Form styling started

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,20 +1,33 @@
 import React from 'react'
 import Link from 'gatsby-link'
+import styled from 'styled-components'
+
+const ContactWrapper = styled.div`
+  place-items: center;
+  display: grid;
+  min-height: 100%;
+  min-width: 100%;
+`
+
+const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+`
 
 const Contact = () => (
-  <div>
+  <ContactWrapper>
     <h1>Contact us</h1>
 
-    <form action="https://formspree.io/davidcooper974@gmail.com" method="POST">
+    <Form action="https://formspree.io/davidcooper974@gmail.com" method="POST">
       <input type="text" name="name" placeholder="Your name" />
       <input type="email" name="_replyto" placeholder="Your email" />
       <textarea type="message" name="message" placeholder="Your message" />
       <input type="submit" value="Send" />
-    </form>
+    </Form>
     <Link to="/">
       <button>Take me home</button>
     </Link>
-  </div>
+  </ContactWrapper>
 )
 
 export default Contact
